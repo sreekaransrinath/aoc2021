94923a61ded4105f1acf17dc9e7ca97a28e2caf8
Sreekaran Srinath <sreekaran.srinath@gmail.com>
2021-12-02 14:30:05 +0530

    ✨ Add Day 2 Statement + Input + Initial Solutions

---
+++ Day 2/archived_solutions.py
@@ -0,0 +1,1 @@
+# Any time a revised solution pops up, the previous solution is archived in this file to keep track of the author's thought process.
---
+++ Day 2/solution.py
@@ -0,0 +1,39 @@
+# Advent of Code 2021 Day 2 Solution
+# Author: Sreekaran
+
+import time
+
+start = time.time()
+with open((__file__.rstrip("solution.py") + "input.txt"), 'r') as input_file:
+    input = input_file.readlines()
+
+print("Part One: ")
+
+x, y = 0, 0
+for line in input:
+    num = int(line.split()[1])
+    if line.startswith('forward'):
+        x += num
+    elif line.startswith('up'):
+        y -= num
+    elif line.startswith('down'):
+        y += num
+
+print(x * y)
+
+print("Part Two: ")
+
+aim, x, y = 0, 0, 0
+for line in input:
+    num = int(line.split()[1])
+    if line.startswith('forward'):
+        x += num
+        y += num * aim
+    elif line.startswith('up'):
+        aim -= num
+    elif line.startswith('down'):
+        aim += num
+
+print(x * y)
+
+print("Time Taken: ", time.time() - start)
