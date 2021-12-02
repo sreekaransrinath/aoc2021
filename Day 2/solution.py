157f56d121b42e4da83fe682d89a6850130dfc3f
Sreekaran Srinath <sreekaran.srinath@gmail.com>
2021-12-02 14:30:59 +0530

    🎨 Formatted solutions

--- Day 2/solution.py
+++ Day 2/solution.py
@@ -4,7 +4,7 @@
 import time
 
 start = time.time()
-with open((__file__.rstrip("solution.py") + "input.txt"), 'r') as input_file:
+with open((__file__.rstrip("solution.py") + "input.txt"), "r") as input_file:
     input = input_file.readlines()
 
 print("Part One: ")
@@ -12,11 +12,11 @@ print("Part One: ")
 x, y = 0, 0
 for line in input:
     num = int(line.split()[1])
-    if line.startswith('forward'):
+    if line.startswith("forward"):
         x += num
-    elif line.startswith('up'):
+    elif line.startswith("up"):
         y -= num
-    elif line.startswith('down'):
+    elif line.startswith("down"):
         y += num
 
 print(x * y)
@@ -26,12 +26,12 @@ print("Part Two: ")
 aim, x, y = 0, 0, 0
 for line in input:
     num = int(line.split()[1])
-    if line.startswith('forward'):
+    if line.startswith("forward"):
         x += num
         y += num * aim
-    elif line.startswith('up'):
+    elif line.startswith("up"):
         aim -= num
-    elif line.startswith('down'):
+    elif line.startswith("down"):
         aim += num
 
 print(x * y)
